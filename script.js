9527d4b7b216539408df002f22c33292a73f373b
wolfgc <33135556+WolfGC@users.noreply.github.com>
2023-02-21 14:12:04 -0500

    Multi-color and Single-color modes functional , variables hold values for DOM manip in script.js

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 let input = 16;
+let colorValue = `#d62828`;
+let modeValue = 'singleColor';
+
+function updateColor(newColor) {
+  colorValue = newColor;
+}
+
+function updateMode(newMode) {
+  setMode(newMode);
+  modeValue = newMode;
+}
+
 const grid = document.querySelector(".grid");
 const newGridBtn = document.querySelector(".new-grid");
+const multiColorBtn = document.querySelector(".multi-color");
+const singleColorBtn = document.querySelector(".single-color");
+const userColor = document.querySelector('.color-picker');
 
+userColor.oninput = (e) => updateColor(e.target.value);
 newGridBtn.onclick = () => newGrid();
+multiColorBtn.onclick = () => updateMode('multiColor');
+singleColorBtn.onclick =() => updateMode('singleColor');
+
+function changeColor(e) {
+  if(modeValue === 'singleColor') {
+  e.target.style.backgroundColor = colorValue;
+} else if (modeValue === 'multiColor') {
+  const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+} 
+}
+
+function setMode(newMode) {
+  if(modeValue === 'multiColor') {
+    multiColorBtn.classList.remove('active');
+  } else if (modeValue === 'singleColor') {
+    singleColorBtn.classList.remove('active');
+  }
+
+  if (newMode === 'multiColor') {
+    multiColorBtn.classList.add('active');
+  } else if (newMode === 'singleColor') {
+    singleColorBtn.classList.add('active');
+  }
+}
 
 function newGrid() {
-  input = document.querySelector(".input").value;
-  clearGrid();
+  handleInput(input);
+  grid.innerHTML = '';
   initGrid(input);
 }
 
-function clearGrid() {
-  grid.innerHTML = "";
+function handleInput() {
+  input = document.querySelector(".input").value;
+  if (input > 100) input = 100;
+  if (input < 10 ) input = 10;
+  return input;
 }
 
 function initGrid(input) {
@@ -24,7 +70,3 @@ function initGrid(input) {
   }
 }
 initGrid(input);
-
-function changeColor(e) {
-  e.target.classList.add("active");
-}
